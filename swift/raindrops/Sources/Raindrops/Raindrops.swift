d3b17ffe7afe8f3cbfa42fb9d4b2e3e097a57e05
Kaleb Hawkins <KalebHawkins@outlook.com>
2020-08-18 12:32:49 -0500

    modified raindrops

--- swift/raindrops/Sources/Raindrops/Raindrops.swift
+++ swift/raindrops/Sources/Raindrops/Raindrops.swift
@@ -7,27 +7,23 @@ struct Raindrops {
         self.numberOfDrops = numberOfDrops
     }
     
+    private static let harmonics = [
+        (3, "Pling"),
+        (5, "Plang"),
+        (7, "Plong")
+    ]
+    
     var sounds: String {
-        let factors = numberOfDrops.getFactors()
-
-        var resultingString = String()
-        
-        if factors.contains(3) {
-            resultingString.append("Pling")
-        }
-        if factors.contains(5) {
-            resultingString.append("Plang")
-        }
-        if factors.contains(7) {
-            resultingString.append("Plong")
+        let resultingString = Raindrops.harmonics.reduce("") {
+            $0 + (numberOfDrops % $1.0 == 0 ? $1.1 : "")
         }
         
-        return resultingString.isEmpty ? String(self.numberOfDrops) : resultingString
+        return resultingString.isEmpty ? String(numberOfDrops) : resultingString
     }
 }
 
 extension Int {
-    func getFactors() -> [Int] {
+    var factors: [Int] {
         return (1...self).filter({ self % $0 == 0 })
     }
 }
