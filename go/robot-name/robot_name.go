2f7d8a522ea9c9a67aa053eb5b78f2a1c4eacfa1
Kaleb Hawkins <KalebHawkins@outlook.com>
2021-11-01 21:08:25 -0500

    optimized robot_name.go iteration.

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -3,46 +3,36 @@ package robotname
 import (
 	"errors"
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 type Robot struct {
 	name string
 }
 
-var validNames = make(map[string]bool)
+var validNames = make([]string, 26*26*10*10*10)
 
 func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
+	var index int = 0
 	for a := 'A'; a <= 'Z'; a++ {
 		for b := 'A'; b <= 'Z'; b++ {
 			for c := 0; c <= 999; c++ {
-				validNames[fmt.Sprintf("%c%c%.3d", a, b, c)] = false
+				validNames[index] = fmt.Sprintf("%c%c%.3d", a, b, c)
+				index++
 			}
 		}
 	}
 }
-
 func (r *Robot) Name() (string, error) {
 	if r.name != "" {
 		return r.name, nil
 	}
-
 	if len(validNames) == 0 {
 		return "", errors.New("namespace exhausted: no more valid names left")
 	}
-
-	for k := range validNames {
-		r.name = k
-		delete(validNames, k)
-		break
-	}
-
+	r.name = validNames[0]
+	validNames = validNames[1:]
 	return r.name, nil
 }
-
 func (r *Robot) Reset() {
 	r.name = ""
 }
