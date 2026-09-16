0183d59357fc431e0fe8125e97323b755e107d4b
Kaleb Hawkins <KalebHawkins@outlook.com>
2026-09-16 14:00:53 -0500

    bug fix for booking up and hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,20 +2,19 @@ package hamming
 
 import (
 	"fmt"
-	"strings"
 )
 
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
-		return 0, fmt.Errorf("invalid sequences: sequences of different length")
+	ar := []rune(a)
+	br := []rune(b)
+
+	if len(ar) != len(br) {
+		return 0, fmt.Errorf("invalid sequences: sequences cannot be of different length")
 	}
 
-	upperA := strings.ToUpper(a)
-	upperB := strings.ToUpper(b)
-
 	var dist int
-	for i, v := range upperA {
-		if v != rune(upperB[i]) {
+	for i := range ar {
+		if ar[i] != br[i] {
 			dist++
 		}
 	}
